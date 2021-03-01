fe2849a8990c636d28f808e33f1a249b685666e9
Joshua Miles <joshua.m@hackerusa.com>
2021-03-01 14:33:48 -0600

    initial

---
+++ index.js
@@ -0,0 +1,52 @@
+function makeObject(url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
+}
+
+function makeItem(url, left, bottom){
+    let object = makeObject(url, left, bottom)
+
+    object.addEventListener('click', () => {
+        object.remove()
+        addItemToInventory(url)
+    })
+}
+
+function makeInventory(){
+    let inventory = document.createElement('div')
+    inventory.style.position = 'fixed'
+    inventory.style.bottom = '0px';
+    inventory.style.left = '0px'
+    inventory.style.width = '100%'
+    inventory.style.height = '100px'
+    inventory.style.display = 'flex'
+    inventory.style.flexDirection = 'row'
+    inventory.style.alignItems = 'center'
+    inventory.style.justifyContent = 'space-evenly'
+    inventory.style.border = '2px solid black'
+    inventory.style.backgroundColor = 'brown'
+    document.body.append(inventory)
+}
+
+function addItemToInventory(url){
+    let inventoryItem = document.createElement('img')
+    inventoryItem.src = url
+    inventory.append(inventoryItem)
+}
+
+makeInventory()
+makeObject('assets/green-character.gif', 100, 250)
+makeObject('assets/tree.png', 200, 450)
+makeObject('assets/pillar.png', 350, 250)
+makeObject('assets/pine-tree.png', 450, 350)
+makeObject('assets/crate.png', 150, 350)
+makeObject('assets/well.png', 500, 575)
+
+makeItem('assets/sword.png', 500, 555)
+makeItem('assets/shield.png', 165, 335)
+makeItem('assets/staff.png', 600, 250)
